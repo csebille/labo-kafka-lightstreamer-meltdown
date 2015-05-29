1e08d4361d2771af3320623b3ec4acb0bb548504
c.sebille <christophe.sebille@ysance.com>
2015-05-29 12:44:45 +0200

    adding SimpleProducer

---
+++ kafka/krackle/krackle-client/src/main/java/com/ysance/kafka/SimpleProducer.java
@@ -0,0 +1,54 @@
+package com.ysance.kafka;
+
+import com.blackberry.krackle.consumer.ConsumerConfiguration;
+import com.blackberry.krackle.consumer.Consumer;
+import com.blackberry.krackle.meta.MetaData;
+import com.blackberry.krackle.producer.Producer;
+import com.blackberry.krackle.producer.ProducerConfiguration;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+public class SimpleConsumer {
+  
+  public static void main(String[] args) {     
+    Properties props = new Properties();
+    props.setProperty("metadata.broker.list","localhost:9092");
+    ProducerConfiguration conf = new ProducerConfiguration(props);
+    Producer producer = new Producer(conf, "clientId", "topic", "key");
+    try {    
+      // Use a byte buffer to store your data, and pass the data by referencing that.
+      byte[] buffer = new byte[1024];
+      while ( fillBuffer() ) { // Get some data in your buffer
+      producer.send(buffer, offset, length);
+    }  finally {
+      producer.close();
+    }
+  }
+  
+}
+
+
+/*       Properties props = new Properties();
+       props.setProperty("metadata.broker.list","localhost:9092");
+       ConsumerConfiguration conf = new ConsumerConfiguration(props);
+       Consumer consumer = new Consumer(conf, "clientId", "test", 0);
+       //Consumer consumer = new Consumer(conf, "clientId", "test");
+
+       // Use a byte buffer to store the message you retrieve.
+       byte[] buffer = new byte[1024];
+       int bytesRead;
+       while ( true ) {
+          bytesRead = consumer.getMessage(buffer, 0, buffer.length);
+          if (bytesRead > 0) {
+           // the first bytesRead bytes of the buffer are the message.
+           System.out.println(new String(buffer,0, bytesRead));
+          }
+       }
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  */
